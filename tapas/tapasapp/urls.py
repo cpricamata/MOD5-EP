a2dfe083f4b064e32f7ba8560031d9edd90585b8
bonbernido <menediobernido@gmail.com>
2026-03-16 16:43:49 +0800

    changed base.html

--- tapas/tapasapp/urls.py
+++ tapas/tapasapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('basic_list', views.view_basic_list, name='view_basic_list'),
     path('', views.view_menu, name='view_menu'),
-    path('add_menu', views.add_menu, name='add_menu')
+    path('add_menu', views.add_menu, name='add_menu'),
+    path('add_menu/', views.add_menu),
 ]
