10497f8b7a13ef53fb7a3688c101570a88b95b13
Giordann <giordann.phoenixx.bautista@student.ateneo.edu>
2026-03-17 13:52:09 +0800

    Adjusted templates, debugging, and pathing

--- M3E-Models/MyInventoryApp/views.py
+++ M3E-Models/MyInventoryApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Supplier
+from .models import WaterBottle
 
 # Create your views here.
+def view_bottles(request):
+    bottle_objects = WaterBottle.objects.all()
+    return render(request, 'MyInventoryApp/bottles.html', {'bottles':bottle_objects})
+
+def view_supplier(request):
+    supplier_objects = Supplier.objects.all()
+    return render(request, 'MyInventoryApp/suppliers.html', {'suppliers':supplier_objects})
--- M3E-Models/MyInventorySystem/urls.py
+++ M3E-Models/MyInventorySystem/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from MyInventoryApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('view_supplier', views.view_supplier, name='view_supplier')
+    path('view_supplier', views.view_supplier, name='view_supplier'),
+    path('view_bottles', views.view_bottles, name='view_bottles')
 ]
