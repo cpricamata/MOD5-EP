0670b53665265401789ae6b6f17a92df278b5028
Giordann <giordann.phoenixx.bautista@student.ateneo.edu>
2026-03-17 16:26:38 +0800

    Added functionality to bottles and add bottles.

--- M3E-Models/MyInventoryApp/views.py
+++ M3E-Models/MyInventoryApp/views.py
@@ -10,3 +10,6 @@ def view_bottles(request):
 def view_supplier(request):
     supplier_objects = Supplier.objects.all()
     return render(request, 'MyInventoryApp/suppliers.html', {'suppliers':supplier_objects})
+
+def add_bottle(request):
+    return render(request, 'MyInventoryApp/add_bottle.html')
--- M3E-Models/MyInventorySystem/urls.py
+++ M3E-Models/MyInventorySystem/urls.py
@@ -22,5 +22,6 @@ from MyInventoryApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('view_supplier', views.view_supplier, name='view_supplier'),
-    path('view_bottles', views.view_bottles, name='view_bottles')
+    path('view_bottles', views.view_bottles, name='view_bottles'),
+    path('add_bottle', views.add_bottle, name='add_bottle')
 ]
